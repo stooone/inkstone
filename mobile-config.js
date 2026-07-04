a537c04bc3e425bd9956691a8399b8d2579953ce
JUHASZ Peter Karoly <stone@midway.hu>
2026-07-04 13:53:59 +0200

    cleanup of old stuff

--- mobile-config.js
+++
@@ -1,7 +0,0 @@
-App.accessRule('https://www.skishore.me/inkstone/*');
-App.info({
-  name: 'Inkstone',
-  description: 'Learn to write Chinese characters.',
-  version: '0.1.4'
-});
-App.setPreference('orientation', 'portrait');
